8b3fc7645056b6e7c6f9b742a41c84c77f2b6876
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-12 00:59:20 +0300

    Wrap the pages with chat & message providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AuthProvider } from "@/context/authContext";
+import { ChatProvider } from "@/context/chatContext";
+import { MessageProvider } from "@/context/messageContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <ChatProvider>
+        <MessageProvider>
+          <Component {...pageProps} />
+        </MessageProvider>
+      </ChatProvider>
     </AuthProvider>
   );
 }
