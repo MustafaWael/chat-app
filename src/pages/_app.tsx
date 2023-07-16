b3a0ed8b91b3b9ec35646906fbcc3589dc210e7f
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 21:55:00 +0300

    Move the AuthConsumer to wrap the app page instead

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import AuthConsumer from "@/HOCs/AuthConsumer";
 import { AuthProvider } from "@/context/authContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -6,9 +5,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <AuthConsumer>
-        <Component {...pageProps} />
-      </AuthConsumer>
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,7 +1,17 @@
+import AuthConsumer from "@/HOCs/AuthConsumer";
+import Layout from "@/components/Layout";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App() {
-  return <main className={`${inter.className}`}>App</main>;
+  return (
+    <AuthConsumer>
+      <main
+        className={`${inter.className} bg-gray-300 h-screen w-full md:p-4 flex gap-x-4 overflow-hidden`}
+      >
+        <Layout />
+      </main>
+    </AuthConsumer>
+  );
 }
