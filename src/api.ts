5e87b7711ac0db03b75c8bc6849e677665a65c9b
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 21:52:35 +0300

    Edit the api to use the NEXT_PUBLIC_API_URL env

--- src/api.ts
+++ src/api.ts
@@ -2,4 +2,4 @@ import axios from "axios";
 
 export const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
-export const api = axios.create({ baseURL: "http://localhost:4000" });
+export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000" });
