c2eac89ded9a35a16b6786fa45410a79e24ecf3c
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 21:55:52 +0300

    Some refactor to index and /signup pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,67 @@
+import LoadingIcon from "@/components/LoadingIcon";
 import { AuthContext } from "@/context/authContext";
 import { Inter } from "next/font/google";
-import { useContext } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const credientials = {
-  email: "mustafa.wael.dev@gmail.com",
-  password: "mustafa00000",
-};
-
-const registerCredientials = {
-  name: "from frontend",
-  email: "from.",
-  password: "frontend",
-};
-
 export default function Home() {
-  const { login, register, logout, user, loading, error } =
-    useContext(AuthContext);
+  const { login, user, loading, error } = useContext(AuthContext);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    login({ email, password });
+  };
+
+  useEffect(() => {
+    if (user) {
+      router.replace("/app");
+    }
+  }, [user, router]);
 
   return (
-    <main className={`${inter.className}`}>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {user && <p>{user.user.name}</p>}
-      <button onClick={() => login(credientials)}>Login</button>
-
-      <br />
-      <br />
-
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {user && <p>{user.user.name}</p>}
-      <button onClick={() => register(registerCredientials)}>Register</button>
-
-      <br />
-      <br />
-
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {user && <p>{user?.user?.name}</p>}
-      <button onClick={logout}>Logout</button>
+    <main
+      className={`${inter.className} w-screen min-h-screen grid place-content-center bg-gray-50 p-4`}
+    >
+      <div className="w-full md:w-96 p-6 bg-white rounded-lg shadow-lg flex flex-col justify-center items-center">
+        <h1 className="text-3xl text-center mb-6">Login</h1>
+        {error && <p className="text-red-500 mb-3">{error}</p>}
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="w-full md:w-80 h-12 border border-gray-300 rounded-lg px-3 mb-3"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="w-full md:w-80 h-12 border border-gray-300 rounded-lg px-3 mb-3"
+          />
+          <button
+            type="submit"
+            className="w-full md:w-80 h-12 bg-blue-500 text-white rounded-lg flex items-center justify-center gap-x-4"
+            disabled={loading}
+          >
+            <span>Login</span>
+            {!user && loading && <LoadingIcon />}
+          </button>
+        </form>
+      </div>
+      <h2 className="text-center">
+        Did not have an account?{" "}
+        <Link href="/signup" className="text-blue-500">
+          Signup
+        </Link>
+      </h2>
     </main>
   );
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,72 @@
+import { AuthContext } from "@/context/authContext";
 import { Inter } from "next/font/google";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Signup() {
-  return <main className={`${inter.className}`}>Signup</main>;
+  const { register, user, loading, error } = useContext(AuthContext);
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    register({ name, email, password });
+  };
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user, router]);
+
+  return (
+    <main
+      className={`${inter.className} w-screen min-h-screen grid place-content-center bg-gray-50 p-4`}
+    >
+      <div className="w-full md:w-96 p-6 bg-white rounded-lg shadow-lg flex flex-col justify-center items-center">
+        <h1 className="text-3xl text-center mb-6">Create an account</h1>
+        {error && <p className="text-red-500 mb-3">{error}</p>}
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <input
+            type="text"
+            name="name"
+            placeholder="Username"
+            className="w-full md:w-80 h-12 border border-gray-300 rounded-lg px-3 mb-3"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="w-full md:w-80 h-12 border border-gray-300 rounded-lg px-3 mb-3"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="w-full md:w-80 h-12 border border-gray-300 rounded-lg px-3 mb-3"
+          />
+          <button
+            type="submit"
+            className="w-full md:w-80 h-12 bg-blue-500 text-white rounded-lg"
+            disabled={loading}
+          >
+            Signup {loading && <span className="ml-2">Loading...</span>}
+          </button>
+        </form>
+      </div>
+      <h2 className="text-center">
+        Did you have an account?{" "}
+        <Link href="/" className="text-blue-500">
+          Signin
+        </Link>
+      </h2>
+    </main>
+  );
 }
