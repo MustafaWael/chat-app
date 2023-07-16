7556f801c0f2845060ad9b64b222d41a791c68c2
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-17 00:54:42 +0300

    change the height to dvh

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -8,7 +8,10 @@ export default function App() {
   return (
     <AuthConsumer>
       <main
-        className={`${inter.className} bg-gray-300 h-screen w-full md:p-4 flex gap-x-4 overflow-hidden`}
+        className={`${inter.className} bg-gray-300 w-full md:p-4 flex gap-x-4 overflow-hidden`}
+        style={{
+          height: "100dvh",
+        }}
       >
         <Layout />
       </main>
